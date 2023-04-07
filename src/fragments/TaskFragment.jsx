2ce17f3790f142bd9ce82046d5b177daa84a781c
mirko06854 <79521685+mirko06854@users.noreply.github.com>
2023-04-07 13:56:33 +0200

    added description input text from material-ui

--- src/fragments/TaskFragment.jsx
+++ src/fragments/TaskFragment.jsx
@@ -1,17 +1,29 @@
 import React, { useState } from 'react';
+import { TextField } from '@mui/material';
 
 export const TaskFragment = ({ task }) => {
   const [completed, setCompleted] = useState(false);
-
+  const [description, setDescription] = useState(task.description);
   const toggleCompleted = () => {
     setCompleted(!completed);
   };
+
+    const handleDescriptionChange = (event) => {
+        setDescription(event.target.value);
+    };
 
   return (
     <div key={task.id} className="task-item">
       <h3>{task.title}</h3>
       <p>User: {task.user}</p>
       <p>Place: {task.place}</p>
+        <TextField
+            label="Description"
+            value={description}
+            onChange={handleDescriptionChange}
+            multiline
+            rows={4}
+        />
       <button onClick={toggleCompleted}>
         {completed ? 'Mark Incomplete' : 'Mark Complete'}
       </button>
@@ -20,4 +32,5 @@ export const TaskFragment = ({ task }) => {
 };
 
 export default TaskFragment;
+
 
