32d2f60b18a96dca3e0bd8268671e9b7d00e5897
acoppe <acoppe@unibz.it>
2023-06-12 18:00:11 +0200

    task list

--- TaskList/src/root.component.js
+++ TaskList/src/root.component.js
@@ -26,28 +26,16 @@ const TasksList = ({ tasks }) => {
       <p>Description: {task.description}</p>
       <p>{task.user === null ? '' : 'User: ' + task.user}</p>
       <p>{task.place === null ? '' : 'Place: ' + task.place}</p>
-      <p>{task.tags === null ? '' : 'Tags: ' + task.tags}</p>
+      <p>{task.tags === null ? '' : 'tags: ' + task.tags}</p>
       <button onClick={() => toggleCompleted(index)}>{task.status === 'DONE' ? 'Mark as Incomplete' : 'Mark as Complete'}</button>
       <br></br><br></br>
     </div>
   ))}
 
-  const dummyTask = (
-    <div>
-      <h3 >DUMMY TASK</h3>
-      <p>Description: I'm not real</p>
-      <p>User: Batman</p>
-      <p>Place: Batcave</p>
-      <p>Tags: BatStuff</p>
-      <button onClick={() => toggleCompleted(0)}>Mark as Complete</button>
-    </div>
-  );
-
   return (
     <div>
       <Box sx={maxHeight}>
         <h2>Tasks</h2>
-        {dummyTask}
         {taskList}
       </Box>
     </div >
